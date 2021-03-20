559ce325cf49eeb68ac98ee18972cc6dc0f0b750
AlvarezJaimeR <jrralvarez@gmail.com>
2021-03-20 13:44:32 -0500

    added a function to filter by age group

--- app.js
+++ app.js
@@ -119,17 +119,12 @@ function searchByCriteria(people){
     case 'age':
       let birthdayList = calculateAge(people);
       console.log(birthdayList);
-      let ageArray = addAgeToDataSet(people, birthdayList);
+      addAgeToDataSet(people, birthdayList);
       console.log(people);
-      console.log(ageArray);
       let ageSearchCriteriaMax = prompt("Input the oldest age you want to see? ");
       let ageSearchCriteriaMin = prompt("Input the youngest age you want to see? ");
-      let sampleAgeList = sortByAge(ageSearchCriteriaMin, ageSearchCriteriaMax, birthdayList);
-      console.log(sampleAgeList);
-/*       let ageList = sortByMinAge(ageSearchCriteriaMin, people);
-      console.log(ageList);
-      finalAgeList = sortyByMaxAge(ageSearchCriteriaMax, ageList)
-      console.log(finalAgeList); */
+      let finalAgeList = sortByAge(ageSearchCriteriaMin, ageSearchCriteriaMax, people);
+      console.log(finalAgeList);
       outputTheNames(finalAgeList);
       break;
   }
@@ -147,34 +142,10 @@ function addAgeToDataSet(people, ageArray){
 //search by age group
 function sortByAge (minAge, maxAge, people){
   let ageBlock = people.filter(function(person){
-    return (minAge < person && person < maxAge);
+    return (minAge < person.age && person.age < maxAge);
   });
   return ageBlock;
 }
-
-/* function sortByMinAge(minAge, people){
-  let count = 0;
-  let ageBlockMin = people.filter(calculateAge(people, count));
-  count ++;
-  return (ageBlockMin > minAge);
-}
-function sortByMaxAge(maxAge, people){  
-  let count = 0;
-let ageBlockFinal = people.filter(calculateAge(people, count));
-  count ++; 
-  return (ageBlockFinal < maxAge);
-} */
-
-  /* for (let i = 0; i < people.data; i++){
-    let currentPersonAge = calculateAge(people, i);
-    if (minAge < currentPersonAge && currentPersonAge < maxAge){
-      let ageBlock = people.filter(function(person){
-        return (minAge < person.dob && person.dob < maxAge);
-      });
-      return ageBlock;
-    }
-  }
-} */
 
 //search by gender
 function sortByGender(genderSearchCriteria, people){
